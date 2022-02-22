d62a6fde4d66e8508d6c02ea4c756e78a90318c8
Dylan <dylan.reeder5@gmail.com>
2022-02-22 18:33:11 -0500

    reversed photos to order from newest to latest

--- components/SliderImages.tsx
+++ components/SliderImages.tsx
@@ -6,13 +6,17 @@ interface Props {
 }
 
 const SliderImages = ({ dates }: Props) => {
+  console.log(dates);
   return (
     <div className="snap-x">
       {dates && dates.length > 0 && (
         <div className="snap-start whitespace-nowrap no-scrollbar scroll-smooth touch-pan-x gap-10 overflow-x-scroll w-full inline-flex snap-mandatory px-20">
-          {dates?.map((img: DateImageType) => (
-            <Photo date={img} key={img.title} />
-          ))}
+          {dates
+            .filter((img: DateImageType) => img.media_type === "image")
+            .reverse()
+            .map((img: DateImageType) => (
+              <Photo date={img} key={img.title} />
+            ))}
         </div>
       )}
     </div>
